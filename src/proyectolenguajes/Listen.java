51dbc081bf3d4e06536f7ff0cb0223a1c43f0f41
thebigchris17 <christian.rodriguez@unitec.edu>
2017-03-07 22:07:38 -0600

    pruebas sphinx v2

--- src/proyectolenguajes/Listen.java
+++ src/proyectolenguajes/Listen.java
@@ -5,71 +5,48 @@
  */
 package proyectolenguajes;
 
-import javax.speech.*;
-import javax.speech.recognition.*;
-import java.io.FileReader;
-import java.util.Locale;
+import edu.cmu.sphinx.api.Configuration;
+import edu.cmu.sphinx.api.LiveSpeechRecognizer;
+import edu.cmu.sphinx.api.SpeechResult;
+import java.io.IOException;
 /**
  *
  * @author Familia Rodriguez
  */
-public class Listen extends ResultAdapter {
+public class Listen {
+    public static void main(String[] args) throws IOException {
+       // Configuration Object
+       Configuration configuration = new Configuration();
 
-    static Recognizer recognizer;
-    String gst;
-
-    @Override
-    public void resultAccepted(ResultEvent re) {
-        try {
-            Result res = (Result) (re.getSource());
-            ResultToken tokens[] = res.getBestTokens();
-
-            String args[] = new String[1];
-            args[0] = "";
-            for (int i = 0; i < tokens.length; i++) {
-                gst = tokens[i].getSpokenText();
-                args[0] += gst + " ";
-                System.out.print(gst + " ");
+       // Set path to the acoustic model.
+       configuration.setAcousticModelPath("resource:/edu/cmu/sphinx/models/en-us/en-us");
+       // Set path to the dictionary.
+       configuration.setDictionaryPath("./7472.dlc");
+       // Set path to the language model.
+       configuration.setLanguageModelPath("./7472.lm");
+       
+       //Recognizer Object, Pass the Configuration object
+        LiveSpeechRecognizer recognize = new LiveSpeechRecognizer(configuration);
+ 
+        //Start Recognition Process (The bool parameter clears the previous cache if true)
+        recognize.startRecognition(true);
+        
+        //Create SpeechResult Object
+        SpeechResult result;
+ 
+        //Checking if recognizer has recognized the speech
+        while ((result = recognize.getResult()) != null) {
+            //Get the recognize speech
+            String command = result.getHypothesis();
+            if(command.equalsIgnoreCase("open file manager")) {
+                System.out.println("File Manager Opened!");
+            } else if (command.equalsIgnoreCase("close file manager")) {
+                System.out.println("File Manager Closed!");
+            } else if (command.equalsIgnoreCase("open browser")) {
+                System.out.println("Browser Opened!");
+            } else if (command.equalsIgnoreCase("close browser")) {
+                System.out.println("Browser Closed!");
             }
-            System.out.println();
-            if (gst.equals("Salir")) {
-                recognizer.deallocate();
-                args[0] = "Nos vimos en MAIAMEEEE";
-                System.out.println(args[0]);
-                //Lee.main(args);
-                System.exit(0);
-            } else {
-                recognizer.suspend();
-                //Lee.main(args);
-                recognizer.resume();
-            }
-        } catch (Exception ex) {
-            System.out.println("Ha ocurrido algo inesperado " + ex);
         }
     }
-
-    public static void main(String args[]) {
-        try {
-            recognizer = Central.createRecognizer(new EngineModeDesc(Locale.ROOT));
-            recognizer.allocate();
-
-            FileReader grammar1 = new FileReader("C:\\Users\\Familia Rodriguez\\Documents\\NetBeansProjects\\ProyectoLenguajes\\Gramatica.txt");
-
-            RuleGrammar rg = recognizer.loadJSGF(grammar1);
-            rg.setEnabled(true);
-
-            recognizer.addResultListener(new Listen());
-
-            System.out.println("Empieze Dictado");
-            recognizer.commitChanges();
-
-            recognizer.requestFocus();
-            recognizer.resume();
-        } catch (Exception e) {
-            System.out.println("Exception en " + e.toString());
-            e.printStackTrace();
-            System.exit(0);
-        }
-    }
-
 }
